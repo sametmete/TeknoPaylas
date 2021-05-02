95bbf30bd4f9ee2da0b5b36375f8682490a1584d
samet mete <samet-mete@hotmail.com>
2021-05-03 00:00:17 +0300

    Add project files.

---
+++ EntityLayer/Concrete/News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class News
+    {
+        public int NewsId { get; set; }
+        public string NewsHeading { get; set; }
+        public string NewsContent { get; set; }
+        public DateTime NewsDate { get; set; }
+        public string NewsImage { get; set; }
+        public bool NewsStatus { get; set; }
+        public bool AdminStatus { get; set; }
+    }
+}
---
+++ TeknoPaylas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeknoPaylas.MvcApplication" Language="C#" %>
